4d55370e05f3998b8157a4e434c38675c068af56
moloch-- <875022+moloch--@users.noreply.github.com>
2022-02-08 08:58:55 -0600

    Converted asm syntax

--- syscalls-asm.h
+++ syscalls-asm.h
@@ -1,81 +1,90 @@
-#pragma once
-#include <windows.h>
-
+#pragma once
+
+#include <windows.h>
+
 #if _WIN64
 
-
-
-#define ZwOpenProcessToken NtOpenProcessToken
-__asm__("NtOpenProcessToken: \n\
-mov [rsp +8], rcx \n\
-mov [rsp+16], rdx \n\
-mov [rsp+24], r8 \n\
-mov [rsp+32], r9 \n\
-sub rsp, 0x28 \n\
-mov ecx, 0x0899D11B4 \n\
-call SW2_GetSyscallNumber \n\
-add rsp, 0x28 \n\
-mov rcx, [rsp +8] \n\
-mov rdx, [rsp+16] \n\
-mov r8, [rsp+24] \n\
-mov r9, [rsp+32] \n\
-mov r10, rcx \n\
-syscall \n\
-ret \n\
+#define ZwOpenProcess NtOpenProcess
+__asm__("NtOpenProcess: \n\
+	push rcx \n\
+	push rdx \n\
+	push r8 \n\
+	push r9 \n\
+	sub rsp, 0x28 \n\
+	call GetSyscallAddress \n\
+	add rsp, 0x28 \n\
+	push rax \n\
+	sub rsp, 0x28 \n\
+	mov ecx, 0xCD9B2A0F \n\
+	call SW2_GetSyscallNumber \n\
+	add rsp, 0x28 \n\
+	pop r11 \n\
+	pop r9 \n\
+	pop r8 \n\
+	pop rdx \n\
+	pop rcx \n\
+	mov r10, rcx \n\
+	jmp r11 \n\
 ");
 
-#define ZwClose NtClose
+#define ZwClose NtClose
 __asm__("NtClose: \n\
-mov [rsp +8], rcx \n\
-mov [rsp+16], rdx \n\
-mov [rsp+24], r8 \n\
-mov [rsp+32], r9 \n\
-sub rsp, 0x28 \n\
-mov ecx, 0x0329A20F3 \n\
-call SW2_GetSyscallNumber \n\
-add rsp, 0x28 \n\
-mov rcx, [rsp +8] \n\
-mov rdx, [rsp+16] \n\
-mov r8, [rsp+24] \n\
-mov r9, [rsp+32] \n\
-mov r10, rcx \n\
-syscall \n\
-ret \n\
+	push rcx \n\
+	push rdx \n\
+	push r8 \n\
+	push r9 \n\
+	sub rsp, 0x28 \n\
+	call GetSyscallAddress \n\
+	add rsp, 0x28 \n\
+	push rax \n\
+	sub rsp, 0x28 \n\
+	mov ecx, 0x2252D33F \n\
+	call SW2_GetSyscallNumber \n\
+	add rsp, 0x28 \n\
+	pop r11 \n\
+	pop r9 \n\
+	pop r8 \n\
+	pop rdx \n\
+	pop rcx \n\
+	mov r10, rcx \n\
+	jmp r11 \n\
 ");
 
-#define ZwAdjustPrivilegesToken NtAdjustPrivilegesToken
+#define ZwAdjustPrivilegesToken NtAdjustPrivilegesToken
+
 __asm__("NtAdjustPrivilegesToken: \n\
-mov [rsp +8], rcx \n\
-mov [rsp+16], rdx \n\
-mov [rsp+24], r8 \n\
-mov [rsp+32], r9 \n\
+push, rcx \n\
+push, rdx \n\
+push, r8 \n\
+push, r9 \n\
 sub rsp, 0x28 \n\
 mov ecx, 0x06DD8BAE2 \n\
 call SW2_GetSyscallNumber \n\
 add rsp, 0x28 \n\
-mov rcx, [rsp +8] \n\
-mov rdx, [rsp+16] \n\
-mov r8, [rsp+24] \n\
-mov r9, [rsp+32] \n\
+pop rcx \n\
+pop rdx \n\
+pop r8 \n\
+pop r9 \n\
 mov r10, rcx \n\
 syscall \n\
 ret \n\
 ");
 
-#define ZwOpenProcess NtOpenProcess
+#define ZwOpenProcess NtOpenProcess
+
 __asm__("NtOpenProcess: \n\
-mov [rsp +8], rcx \n\
-mov [rsp+16], rdx \n\
-mov [rsp+24], r8 \n\
-mov [rsp+32], r9 \n\
+push, rcx \n\
+push, rdx \n\
+push, r8 \n\
+push, r9 \n\
 sub rsp, 0x28 \n\
 mov ecx, 0x0C9AAC628 \n\
 call SW2_GetSyscallNumber \n\
 add rsp, 0x28 \n\
-mov rcx, [rsp +8] \n\
-mov rdx, [rsp+16] \n\
-mov r8, [rsp+24] \n\
-mov r9, [rsp+32] \n\
+pop rcx \n\
+pop rdx \n\
+pop r8 \n\
+pop r9 \n\
 mov r10, rcx \n\
 syscall \n\
 ret \n\
